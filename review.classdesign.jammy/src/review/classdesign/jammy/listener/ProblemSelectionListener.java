8b66e4504e446a63a36e0871e89df978e7a33694
fv <fv@192.168.1.30>
2015-10-07 14:04:32 +0200

    Adds listeners for contest / problem selection.

---
+++ review.classdesign.jammy/src/review/classdesign/jammy/listener/ProblemSelectionListener.java
@@ -0,0 +1,17 @@
+package review.classdesign.jammy.listener;
+
+import review.classdesign.jammy.model.Problem;
+
+/**
+ * 
+ * @author fv
+ */
+public interface ProblemSelectionListener {
+
+	/**
+	 * 
+	 * @param problem
+	 */
+	void problemSelected(Problem problem);
+
+}
---
+++ review.classdesign.jammy/src/review/classdesign/jammy/listener/RoundSelectionListener.java
@@ -0,0 +1,17 @@
+package review.classdesign.jammy.listener;
+
+import review.classdesign.jammy.model.Round;
+
+/**
+ * 
+ * @author fv
+ */
+public interface RoundSelectionListener {
+
+	/**
+	 * 
+	 * @param round
+	 */
+	void roundSelected(final Round round);
+
+}
