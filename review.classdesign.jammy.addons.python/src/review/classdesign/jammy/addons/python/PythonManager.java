a7c6246de699fd376773d896c8b697da7d8f7216
fv <fv@192.168.1.30>
2015-10-23 13:35:04 +0200

    Implements a basic Jammy addons for supporting python language through process manipulation.

---
+++ review.classdesign.jammy.addons.python/src/review/classdesign/jammy/addons/python/PythonManager.java
@@ -0,0 +1,32 @@
+package review.classdesign.jammy.addons.python;
+
+import review.classdesign.jammy.addons.AbstractProcessManager;
+
+/**
+ * 
+ * @author fv
+ */
+public final class PythonManager extends AbstractProcessManager {
+
+	/** **/
+	private static final String SOLVER_EXTENSION = ".py";
+
+	/** {@inheritDoc} **/
+	@Override
+	public String getTemplate(final String name) {
+		return "";
+	}
+
+	/** {@inheritDoc} **/
+	@Override
+	public String getExtension() {
+		return SOLVER_EXTENSION;
+	}
+
+	/** {@inheritDoc} **/
+	@Override
+	protected String getCommand() {
+		return "python";
+	}
+
+}
---
+++ review.classdesign.jammy.addons.python/src/review/classdesign/jammy/addons/python/PythonRunner.java
@@ -0,0 +1,58 @@
+package review.classdesign.jammy.addons.python;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+
+import review.classdesign.jammy.addons.ISolverRunner;
+import review.classdesign.jammy.core.ProblemSolver;
+
+/**
+ * 
+ * @author fv
+ */
+public final class PythonRunner implements ISolverRunner {
+
+	/** **/
+	private static final String PYTHON_COMMAND = "python";
+
+	/** **/
+	private final ProblemSolver solver;
+
+	/** **/
+	private Process process;
+
+	/**
+	 * 
+	 * @param solver
+	 */
+	public PythonRunner(final ProblemSolver solver) {
+		this.solver = solver;
+	}
+
+	/** {@inheritDoc} **/
+	@Override
+	public boolean isTerminated() {
+		return (process != null && !process.isAlive());
+	}
+
+	/** {@inheritDoc} **/
+	@Override
+	public void run(final String arguments, final String output) throws CoreException {
+		final String path = solver.getFile().getLocation().toOSString();
+		final List<String> command = Arrays.asList(PYTHON_COMMAND, path, arguments);
+		final ProcessBuilder builder = new ProcessBuilder(command);
+		builder.redirectOutput(new File(output));
+		try {
+			process = builder.start();
+		}
+		catch (final IOException e) {
+			// TODO : Throw error.
+			e.printStackTrace();
+		}
+	}
+
+}
