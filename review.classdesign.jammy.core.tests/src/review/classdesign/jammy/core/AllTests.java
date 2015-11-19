41a90cf303b122e168a84a6a5d32f17e4fef9874
Faylixe <m8uxatqu#>
2015-11-19 08:39:29 +0100

    Initializes core tests fragment project.

---
+++ review.classdesign.jammy.core.tests/src/review/classdesign/jammy/core/AllTests.java
@@ -0,0 +1,13 @@
+package review.classdesign.jammy.core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	
+})
+public final class AllTests {
+
+}
